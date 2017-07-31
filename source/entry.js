00ea758dba3e9657df8a5edebf440203c5671153
Mike King <yo@mikeking.io>
2017-07-31 01:21:59 -1000

    updating webpack config

---
+++ source/entry.js
@@ -0,0 +1,10 @@
+/**
+ * Main application entry point
+ */
+
+// Application Styles
+import './scss/style.scss';
+
+// Application Scripts
+import './js/vendor';
+import './js/app';
---
+++ source/js/app.js
@@ -0,0 +1,16 @@
+/**
+ *  Main Application file
+ */
+
+export default class App {
+
+  constructor(){
+
+    console.log('yo!');
+  
+  }
+
+}
+
+/** hello.world */
+new App();
